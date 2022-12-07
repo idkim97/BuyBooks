c7590b0d69640e8c1c5381d358d29284ef01034e
idkim97 <idkim9797@naver.com>
2022-12-07 23:03:26 +0900

    Update MemberController.java

--- src/main/java/com/vam/controller/MemberController.java
+++ src/main/java/com/vam/controller/MemberController.java
@@ -1,8 +1,14 @@
 package com.vam.controller;
 
+import java.util.Random;
+
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,6 +25,9 @@ public class MemberController {
 
 	@Autowired
 	private MemberService memberService;
+	
+	@Autowired
+	private JavaMailSender mailSender;
 
 	// 회원가입 페이지 이동
 	@RequestMapping(value = "join", method = RequestMethod.GET)
@@ -73,9 +82,45 @@ public class MemberController {
 	//이메일 인증
 	@RequestMapping(value="/mailCheck", method=RequestMethod.GET)
 	@ResponseBody
-	public void mailCheckGET(String email) throws Exception{
+	public String mailCheckGET(String email) throws Exception{
 		logger.info("이메일 데이터 전송확인");
 		logger.info("인증번호 : " + email);
+		
+		/* 인증번호(난수)생성*/
+		Random random = new Random();
+		// 111111~999999 범위의 숫자를 얻기위함
+		int checkNum = random.nextInt(888888)+111111;
+		logger.info("인증번호 " + checkNum);
+		
+		
+		/* 이메일 보내기 */
+		
+		// root-context.xml에 삽입한 자신의 이메일 계정 주소
+		String setFrom = "idkim9797@naver.com";
+		// 수신받을 이메일
+		String toMail = email;
+		// 이메일 제목
+		String title = "회원가입 인증 이메일입니다.";
+		// 이메일 내용
+		String content = "홈페이지를 방문해주셔서 감사합니다." + "<br><br>" + "인증 번호는 " + checkNum + "입니다." + "<br>" + "해당 인증번호를 인증번호 확인란에 기입하여 주세요.";
+		
+		
+		try {
+			MimeMessage message = mailSender.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(message,true,"utf-8");
+			helper.setFrom(setFrom);
+			helper.setTo(toMail);
+			helper.setSubject(title);
+			helper.setText(content,true);
+			mailSender.send(message);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		String num = Integer.toString(checkNum);
+		
+		return num;
 	}
 
 }
