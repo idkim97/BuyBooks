e1bc700a3a9b9b5224552fb5dbfea9750e26ceee
idkim97 <idkim9797@naver.com>
2022-12-11 01:03:32 +0900

    Update MemberServiceImpl.java

--- src/main/java/com/vam/service/MemberServiceImpl.java
+++ src/main/java/com/vam/service/MemberServiceImpl.java
@@ -12,13 +12,22 @@ public class MemberServiceImpl implements MemberService {
 	@Autowired
 	MemberMapper memberMapper;
 
+	// 회원가입
 	@Override
 	public void memberJoin(MemberVO member) throws Exception{
 		memberMapper.memberJoin(member);
 	}
 	
+	// 아이디 중복 검사
 	@Override
 	public int idCheck(String memberId) throws Exception{
 		return memberMapper.idCheck(memberId);
+	}
+	
+	
+	// 로그인
+	@Override
+	public MemberVO memberLogin(MemberVO member) throws Exception {
+		return memberMapper.memberLogin(member);
 	}
 }
