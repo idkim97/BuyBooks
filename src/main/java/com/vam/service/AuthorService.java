16abf4b8a85f0c136ca779477d91912f343562ac
idkim97 <idkim9797@naver.com>
2022-12-28 17:03:40 +0900

    Update AuthorService.java

--- src/main/java/com/vam/service/AuthorService.java
+++ src/main/java/com/vam/service/AuthorService.java
@@ -1,9 +1,15 @@
 package com.vam.service;
 
+import java.util.List;
+
 import com.vam.model.AuthorVO;
+import com.vam.model.Criteria;
 
 public interface AuthorService {
 	
-	//작가 등록
+	// 작가 등록
 	public void authorEnroll(AuthorVO author) throws Exception;
+	
+	// 작가 목록
+	public List<AuthorVO> authorGetList(Criteria cri) throws Exception;
 }
