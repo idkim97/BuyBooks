0fb1a628c7c0b43efeab3b40beb00700a4dadd5a
idkim97 <idkim9797@naver.com>
2023-01-03 21:46:42 +0900

    Update AuthorMapperTests.java

--- src/test/java/com/vam/mapper/AuthorMapperTests.java
+++ src/test/java/com/vam/mapper/AuthorMapperTests.java
@@ -48,6 +48,7 @@ public class AuthorMapperTests {
 	}
 	*/
 	
+	/*
 	// 작가 총 수
 	@Test
 	public void authorGetTotalTest() throws Exception{
@@ -58,4 +59,38 @@ public class AuthorMapperTests {
 		
 		System.out.println("total..........." + total);
 	}
+	*/
+	
+	
+	/*
+	// 작가 상세 페이지
+	@Test
+	public void authorGetDetailTest() {
+		int authorId = 30;
+		AuthorVO author = mapper.authorGetDetail(authorId);
+		System.out.println("author......." + author);
+	}
+	*/
+	
+	// 작가 정보 수정
+	@Test
+	public void authorModifyTest() {
+		AuthorVO author = new AuthorVO();
+		
+		author.setAuthorId(1);
+		System.out.println("수정전............" + mapper.authorGetDetail(author.getAuthorId()));
+		
+		author.setAuthorName("수정");
+		author.setNationId("01");
+		author.setAuthorIntro("소개 수정");
+		
+		mapper.authorModify(author);
+		System.out.println("수정후............" + mapper.authorGetDetail(author.getAuthorId()));
+		
+	}
+	
+	
+	
+	
+	
 }
