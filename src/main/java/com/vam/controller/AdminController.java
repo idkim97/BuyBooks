6062c89af09383455202bd4004049bb372819a18
idkim97 <idkim9797@naver.com>
2023-01-03 21:46:18 +0900

    Update AdminController.java

--- src/main/java/com/vam/controller/AdminController.java
+++ src/main/java/com/vam/controller/AdminController.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -78,6 +80,31 @@ public class AdminController {
 		logger.info("authorEnroll : " + author);
 		authorService.authorEnroll(author);		// 작가 등록 쿼리 수행
 		rttr.addFlashAttribute("enroll_result", author.getAuthorName()); // 등록 성공 메세지
+		return "redirect:/admin/authorManage";
+	}
+	
+	// 작가 상세 페이지
+	@GetMapping({"/authorDetail","/authorModify"})
+	public void authorGetInfoGET(int authorId, Criteria cri, Model model) throws Exception{
+		logger.info("authorDetail......." + authorId);
+		
+		// 작가 관리 페이지 정보
+		model.addAttribute("cri",cri);
+		
+		// 선택 작가 정보
+		model.addAttribute("authorInfo",authorService.authorGetDetail(authorId));
+	}
+	
+	// 작가 정보 수정
+	@PostMapping("/authorModify")
+	public String authorModifyPOST(AuthorVO author, RedirectAttributes rttr) throws Exception{
+		
+		logger.info("authorModifyPOST......." + author);
+		
+		int result = authorService.authorModify(author);
+		
+		rttr.addFlashAttribute("modify_result",result);
+		
 		return "redirect:/admin/authorManage";
 	}
 	
