fe7273c79029e338dc31423490c21cb65bc2dcd9
idkim97 <idkim9797@naver.com>
2023-01-08 23:12:07 +0900

    Update AdminController.java

--- src/main/java/com/vam/controller/AdminController.java
+++ src/main/java/com/vam/controller/AdminController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vam.model.AuthorVO;
 import com.vam.model.BookVO;
 import com.vam.model.Criteria;
@@ -47,8 +48,20 @@ public class AdminController {
 	
 	// 상품 등록 페이지 접속
 	@RequestMapping(value="goodsEnroll", method = RequestMethod.GET)
-	public void goodsEnrollGET() throws Exception{
+	public void goodsEnrollGET(Model model) throws Exception{
 		logger.info("상품 등록 페이지 접속");
+		
+		ObjectMapper objm = new ObjectMapper();
+		
+		List list = adminService.cateList();
+		
+		// 전달받은 카테고리 리스트를 String타입의 JSON형식 데이터로 변환
+		String cateList = objm.writeValueAsString(list);
+		
+		model.addAttribute("cateList",cateList);
+		
+		logger.info("변경 전........" + list);
+		logger.info("변경 후........" + cateList);
 	}
 	
 	// 작가 등록 페이지 접속
@@ -125,6 +138,26 @@ public class AdminController {
 		rttr.addFlashAttribute("enroll_result", book.getBookName());
 		
 		return "redirect:/admin/goodsManage";
+	}
+	
+	
+	// 작가 검색 팝업창
+	@GetMapping("/authorPop")
+	public void authorPopGET(Criteria cri, Model model) throws Exception{
+		logger.info("authorPopGET.......");
+		
+		cri.setAmount(5);
+		
+		/* 게시물 출력 데이터 */
+		List list = authorService.authorGetList(cri);
+		if(!list.isEmpty()) {
+			model.addAttribute("list",list);			// 작가 존재 경우
+		}else {
+			model.addAttribute("listCheck","empty");	// 작가 존재하지 않을 경우
+		}
+		
+		/* 페이지 이동 인터페이스 데이터 */
+		model.addAttribute("pageMaker", new PageDTO(cri, authorService.authorGetTotal(cri)));
 	}
 	
 }
