20427d60dce5ff068e522e6170e75066a7036fbb
idkim97 <idkim9797@naver.com>
2023-01-09 21:46:23 +0900

    Update AdminMapper.java

--- src/main/java/com/vam/mapper/AdminMapper.java
+++ src/main/java/com/vam/mapper/AdminMapper.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.vam.model.BookVO;
 import com.vam.model.CateVO;
+import com.vam.model.Criteria;
 
 public interface AdminMapper {
 	
@@ -12,4 +13,13 @@ public interface AdminMapper {
 	
 	// 카테고리 리스트
 	public List<CateVO> cateList();
+	
+	// 상품 리스트
+	public List<BookVO> goodsGetList(Criteria cri);
+	
+	// 상품 총 개수
+	public int goodsGetTotal(Criteria cri);
+	
+	// 상품 조회 페이지
+	public BookVO goodsGetDetail(int bookId);
 }
