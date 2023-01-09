d9edbd76b559adb9c27b06affb3bfd72b665d669
idkim97 <idkim9797@naver.com>
2023-01-09 21:46:43 +0900

    Update AdminMapperTests.java

--- src/test/java/com/vam/mapper/AdminMapperTests.java
+++ src/test/java/com/vam/mapper/AdminMapperTests.java
@@ -1,5 +1,7 @@
 package com.vam.mapper;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.vam.model.BookVO;
+import com.vam.model.Criteria;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
@@ -15,6 +18,7 @@ public class AdminMapperTests {
 	@Autowired
 	private AdminMapper mapper;
 	
+	/*
 	// 상품 등록
 	@Test
 	public void bookEnrollTest() throws Exception{
@@ -34,4 +38,45 @@ public class AdminMapperTests {
 		
 		mapper.bookEnroll(book);
 	}
+	*/
+	
+	/*
+	// 카테고리 리스트
+	@Test
+	public void cateListTest() throws Exception{
+		System.out.println("cateList()........" + mapper.cateList());
+	}
+	*/
+	
+	/*
+	// 상품 리스트 & 상품 총 갯수
+	@Test
+	public void goodsGetListTest() {
+		
+		Criteria cri = new Criteria();
+		
+		// 검색조건 
+		cri.setKeyword("테스트");
+		
+		// 검색 리스트 
+		List list = mapper.goodsGetList(cri);
+		for(int i = 0; i < list.size(); i++) {
+			System.out.println("result......." + i + " : " + list.get(i));
+		}
+		
+		// 상품 총 갯수 
+		int result = mapper.goodsGetTotal(cri);
+		System.out.println("result.........." + result);
+	}
+	*/
+	
+	@Test
+	public void goodsGetDetailTest() {
+		int bookId = 14;
+		BookVO result = mapper.goodsGetDetail(bookId);
+		System.out.println("상품 조회 데이터 : " + result);
+	}
+	
+	
+	
 }
