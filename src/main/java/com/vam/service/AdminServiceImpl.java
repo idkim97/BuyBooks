00a95544d4e0275e5e2021cc4d5ce4d75bd8fb03
idkim97 <idkim9797@naver.com>
2023-01-12 20:11:51 +0900

    Update AdminServiceImpl.java

--- src/main/java/com/vam/service/AdminServiceImpl.java
+++ src/main/java/com/vam/service/AdminServiceImpl.java
@@ -56,4 +56,18 @@ public class AdminServiceImpl implements AdminService {
 		log.info("(service)bookGetDetail......." + bookId);
 		return adminMapper.goodsGetDetail(bookId);
 	}
+
+
+	@Override
+	public int goodsModify(BookVO vo) {
+		log.info("goodsModify.........");
+		return adminMapper.goodsModify(vo);
+	}
+
+
+	@Override
+	public int goodsDelete(int bookId) {
+		log.info("goodsDelete.........");
+		return adminMapper.goodsDelete(bookId);
+	}
 }
