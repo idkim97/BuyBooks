9760ac78a29f0fdf37de284ae97f3ed95b7eadba
idkim97 <idkim9797@naver.com>
2023-01-14 03:34:28 +0900

    Update AdminController.java

--- src/main/java/com/vam/controller/AdminController.java
+++ src/main/java/com/vam/controller/AdminController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -177,6 +178,28 @@ public class AdminController {
 		return "redirect:/admin/authorManage";
 	}
 	
+	// 작가 정보 삭제
+	@PostMapping("/authorDelete")
+	public String authorDeletePOST(int authorId, RedirectAttributes rttr) {
+		logger.info("authorDeletePOST.........");
+		
+		int result=0;
+		
+		try {
+			result = authorService.authorDelete(authorId);
+		}catch(Exception e) {
+			e.printStackTrace();
+			result=2;
+			rttr.addFlashAttribute("delete_result",result);
+			
+			return "redirect:/admin/authorManage";
+		}
+		
+		rttr.addFlashAttribute("delete_result",result);
+		
+		return "redirect:/admin/authorManage";
+	}
+	
 	
 	// 상품 등록
 	@PostMapping("/goodsEnroll")
@@ -209,6 +232,29 @@ public class AdminController {
 		
 		/* 페이지 이동 인터페이스 데이터 */
 		model.addAttribute("pageMaker", new PageDTO(cri, authorService.authorGetTotal(cri)));
+	}
+	
+	// 첨부 파일 업로드
+	@PostMapping("/uploadAjaxAction")
+	public void uploadAjaxActionPOST(MultipartFile[] uploadFile) {
+		
+		logger.info("uploadAjaxActionPOST........");
+		
+		// 향상된 for
+		for(MultipartFile multipartFile : uploadFile) {
+			logger.info("-----------------------------------------------");
+			logger.info("파일 이름 : " + multipartFile.getOriginalFilename());
+			logger.info("파일 타입 : " + multipartFile.getContentType());
+			logger.info("파일 크기 : " + multipartFile.getSize()); 
+		}
+		
+		// 기본 for
+		for (int i = 0; i < uploadFile.length; i++) {
+			logger.info("-----------------------------------------------");
+			logger.info("파일 이름 : " + uploadFile[i].getOriginalFilename());
+			logger.info("파일 타입 : " + uploadFile[i].getContentType());
+			logger.info("파일 크기 : " + uploadFile[i].getSize());
+		}
 	}
 	
 }
