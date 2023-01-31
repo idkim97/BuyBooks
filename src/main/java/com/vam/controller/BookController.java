92e968b796317cb5f8cacbf98ecf763ff41161b4
idkim97 <idkim9797@naver.com>
2023-02-01 03:11:47 +0900

    Update BookController.java

--- src/main/java/com/vam/controller/BookController.java
+++ src/main/java/com/vam/controller/BookController.java
@@ -13,13 +13,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.vam.model.AttachImageVO;
+import com.vam.model.BookVO;
+import com.vam.model.Criteria;
+import com.vam.model.PageDTO;
 import com.vam.service.AttachService;
+import com.vam.service.BookService;
 
 @Controller
 public class BookController {
@@ -28,6 +33,9 @@ public class BookController {
 	
 	@Autowired
 	private AttachService attachService;
+	
+	@Autowired
+	private BookService bookService;
 	
 	//메인 페이지 이동
 	@RequestMapping(value = "/main", method = RequestMethod.GET)
@@ -66,6 +74,27 @@ public class BookController {
 		
 		return new ResponseEntity<List<AttachImageVO>>(attachService.getAttachList(bookId),HttpStatus.OK);
 	}
+	
+	// 상품 검색
+	@GetMapping("search")
+	public String searchGoodsGET(Criteria cri, Model model) {
+		logger.info("cri : " + cri);
+		
+		List<BookVO> list = bookService.getGoodsList(cri);
+		logger.info("pre list : " + list);
+		if(!list.isEmpty()) {
+			model.addAttribute("list",list);
+			logger.info("list : " +list);
+		}else {
+			model.addAttribute("listcheck", "empty");
+			return "search";
+		}
+		
+		model.addAttribute("pageMaker", new PageDTO(cri, bookService.goodsGetTotal(cri)));
+		
+		return "search";
+	}
+	
 	
 	
 
