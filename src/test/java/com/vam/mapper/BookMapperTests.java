8644e3caf9a5defeae06d31671e0785e03d9c5b9
idkim97 <idkim9797@naver.com>
2023-02-01 03:12:11 +0900

    Create BookMapperTests.java

---
+++ src/test/java/com/vam/mapper/BookMapperTests.java
@@ -0,0 +1,55 @@
+package com.vam.mapper;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.vam.model.BookVO;
+import com.vam.model.Criteria;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+public class BookMapperTests {
+	
+	@Autowired
+	private BookMapper mapper;
+	
+	/*
+	@Test
+	public void getGoodsListTest() {
+		
+		Criteria cri = new Criteria();
+		
+		System.out.println("cri : " + cri);
+		
+		List<BookVO> list = mapper.getGoodsList(cri);
+		System.out.println("list : " + list);
+		
+		System.out.println("==============");
+		
+		int goodsTotal = mapper.goodsGetTotal(cri);
+		System.out.println("total : " + goodsTotal);
+	}
+	*/
+	
+	// 작가 id 리스트 요청 
+	@Test
+	public void getAuthorId() {
+		
+		String keyword = "김지";
+		
+		String[] list = mapper.getAuthorIdList(keyword);
+		
+		System.out.println("결과 : " + list.toString());
+		
+		for(String id : list) {
+			System.out.println("개별 결과 : " + id);
+		}
+		
+		
+	}
+}
