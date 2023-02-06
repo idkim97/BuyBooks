07f4c62034c9b2c2221b93fbd9f1e8093912ee38
idkim97 <idkim9797@naver.com>
2023-02-07 02:30:32 +0900

    Update testBatch.java

--- src/main/java/com/vam/task/testBatch.java
+++ src/main/java/com/vam/task/testBatch.java
@@ -9,9 +9,11 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class testBatch {
 
+	/*
 	@Scheduled(cron = "0 * * * * *")
 	public void testMethod() throws Exception{
 		log.warn("배치 실행 테스트.......");
 		log.warn("==================================");
 	}
+	*/
 }
