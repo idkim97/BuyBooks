b4a73506c5625e95b9f57852312442f9f0869ddf
idkim97 <idkim9797@naver.com>
2023-02-13 22:26:44 +0900

    Update BookService.java

--- src/main/java/com/vam/service/BookService.java
+++ src/main/java/com/vam/service/BookService.java
@@ -3,6 +3,7 @@ package com.vam.service;
 import java.util.List;
 
 import com.vam.model.BookVO;
+import com.vam.model.CateVO;
 import com.vam.model.Criteria;
 
 public interface BookService {
@@ -12,4 +13,10 @@ public interface BookService {
 	
 	// 상품 총 개수
 	public int goodsGetTotal(Criteria cri);
+	
+	// 국내 카테고리 리스트
+	public List<CateVO> getCateCode1();
+	
+	// 국외 카테고리 리스트
+	public List<CateVO> getCateCode2();
 }
