7fbebaa04a67389b3eb135c6da7dbe44bf3d4bc3
idkim97 <idkim9797@naver.com>
2023-03-02 17:18:05 +0900

    Create CartMapper.java

---
+++ src/main/java/com/vam/mapper/CartMapper.java
@@ -0,0 +1,24 @@
+package com.vam.mapper;
+
+import java.util.List;
+
+import com.vam.model.CartDTO;
+
+public interface CartMapper {
+	
+	/* 장바구니 추가 */
+	public int addCart(CartDTO cart) throws Exception;
+	
+	/* 장바구니 삭제 */
+	public int deleteCart(int cartId);
+	
+	/* 장바구니 수량 수정 */
+	public int modifyCount(CartDTO cart);
+	
+	/* 장바구니 목록 */
+	public List<CartDTO> getCart(String memberId);	
+	
+	/* 장바구니 확인 */
+	public CartDTO checkCart(CartDTO cart);
+
+}
