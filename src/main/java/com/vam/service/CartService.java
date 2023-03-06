edae1f7237372ab9d15c2ae1dcee155854faf7c6
idkim97 <idkim9797@naver.com>
2023-03-06 21:42:06 +0900

    Update CartService.java

--- src/main/java/com/vam/service/CartService.java
+++ src/main/java/com/vam/service/CartService.java
@@ -11,4 +11,10 @@ public interface CartService {
 	
 	// 장바구니 정보 리스트
 	public List<CartDTO> getCartList(String memberId);
+	
+	// 장바구니 수량 수정
+	public int modifyCount(CartDTO cart);
+	
+	// 장바구니 삭제
+	public int deleteCart(int cartId);
 }
