947ef38375343a72727146422bb7821244482ce5
idkim97 <idkim9797@naver.com>
2023-03-06 21:42:09 +0900

    Update CartServiceImpl.java

--- src/main/java/com/vam/service/CartServiceImpl.java
+++ src/main/java/com/vam/service/CartServiceImpl.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.vam.mapper.AttachMapper;
 import com.vam.mapper.CartMapper;
+import com.vam.model.AttachImageVO;
 import com.vam.model.CartDTO;
 
 @Service
@@ -13,7 +15,12 @@ public class CartServiceImpl implements CartService{
 	
 	@Autowired
 	private CartMapper cartMapper;
+	
+	@Autowired
+	private AttachMapper attachMapper;
 
+	
+	// 장바구니 추가
 	@Override
 	public int addCart(CartDTO cart) {
 		
@@ -31,6 +38,7 @@ public class CartServiceImpl implements CartService{
 		}
 	}
 
+	// 장바구니 리스트 가져오기
 	@Override
 	public List<CartDTO> getCartList(String memberId) {
 
@@ -38,10 +46,28 @@ public class CartServiceImpl implements CartService{
 		
 		for(CartDTO dto : cart) {
 			dto.initSaleTotal();
+			
+			// 이미지 정보 얻기
+			int bookId = dto.getBookId();
+			List<AttachImageVO> imageList = attachMapper.getAttachList(bookId);
+			dto.setImageList(imageList);
 		}
 		return cart;
 	}
 	
+	// 장바구니 수량 수정
+	@Override
+	public int modifyCount(CartDTO cart) {
+		
+		return cartMapper.modifyCount(cart);
+	}
+	
+	// 장바구니 삭제
+	@Override
+	public int deleteCart(int cartId) {
+
+		return cartMapper.deleteCart(cartId);
+	}
 	
 
 }
