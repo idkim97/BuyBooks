2556969d62c8124d2eedf74904b0a9be6fc20b9b
idkim97 <idkim9797@naver.com>
2023-03-07 00:51:30 +0900

    Create OrderController.java

---
+++ src/main/java/com/vam/controller/OrderController.java
@@ -0,0 +1,34 @@
+package com.vam.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.vam.model.OrderPageDTO;
+import com.vam.service.MemberService;
+import com.vam.service.OrderService;
+
+@Controller
+public class OrderController {
+	
+	@Autowired
+	private OrderService orderService;
+	
+	@Autowired
+	private MemberService memberService;
+	
+	
+	
+	@GetMapping("/order/{memberId}")
+	public String orderPgaeGET(@PathVariable("memberId") String memberId, OrderPageDTO opd, Model model) {
+		
+		model.addAttribute("orderList", orderService.getGoodsInfo(opd.getOrders()));
+		model.addAttribute("memberInfo", memberService.getMemberInfo(memberId));
+		
+		return "/order";
+		
+	}
+
+}
