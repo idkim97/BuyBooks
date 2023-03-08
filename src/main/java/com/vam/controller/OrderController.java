9169a0c5e4f4535707877842713c7c87a0d7fb83
idkim97 <idkim9797@naver.com>
2023-03-08 23:05:41 +0900

    Update OrderController.java

--- src/main/java/com/vam/controller/OrderController.java
+++ src/main/java/com/vam/controller/OrderController.java
@@ -1,11 +1,17 @@
 package com.vam.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.vam.model.MemberVO;
+import com.vam.model.OrderDTO;
 import com.vam.model.OrderPageDTO;
 import com.vam.service.MemberService;
 import com.vam.service.OrderService;
@@ -29,6 +35,31 @@ public class OrderController {
 		
 		return "/order";
 		
+	}
+	
+	@PostMapping("/order")
+	public String orderPagePost(OrderDTO od, HttpServletRequest request) {
+		
+		System.out.println(od);
+		
+		orderService.order(od);
+		
+		MemberVO member = new MemberVO();
+		member.setMemberId(od.getMemberId());
+		
+		HttpSession session = request.getSession();
+		
+		try {
+			MemberVO memberLogin = memberService.memberLogin(member);
+			memberLogin.setMemberPw("");
+			session.setAttribute("member", memberLogin);
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		
+		return "redirect:/main";
 	}
 
 }
