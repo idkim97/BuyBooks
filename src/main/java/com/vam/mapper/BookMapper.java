7dfb95d9646b566d054e1f17e752684909b08dbe
idkim97 <idkim9797@naver.com>
2023-03-12 21:59:26 +0900

    Update BookMapper.java

--- src/main/java/com/vam/mapper/BookMapper.java
+++ src/main/java/com/vam/mapper/BookMapper.java
@@ -6,6 +6,7 @@ import com.vam.model.BookVO;
 import com.vam.model.CateFilterDTO;
 import com.vam.model.CateVO;
 import com.vam.model.Criteria;
+import com.vam.model.SelectDTO;
 
 public interface BookMapper {
 
@@ -32,4 +33,10 @@ public interface BookMapper {
 	
 	// 상품 정보
 	public BookVO getGoodsInfo(int bookId);
+	
+	// 리뷰를 위한 상품 id 이름
+	public BookVO getBookIdName(int bookId);
+	
+	// 평점 순 상품 정보
+	public List<SelectDTO> likeSelect();
 }
